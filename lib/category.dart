d1147e6505babe5e4c00bf5a6cd9b689f951fa91
DJ_Kovrik <djkovrik@gmail.com>
2019-01-14 15:19:36 +0300

    Remove comments

--- lib/category.dart
+++ lib/category.dart
@@ -1,28 +1,11 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// To keep your imports tidy, follow the ordering guidelines at
-// https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
-
-/// A custom [Category] widget.
-///
-/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
-/// a colored [InkWell] animation.
 
 final _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 final _iconSize = 60.0;
 
 class Category extends StatelessWidget {
-  /// Creates a [Category].
-  ///
-  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
-  /// the UI, and the icon that represents it (e.g. a ruler).
-  // DONE: You'll need the name, color, and iconLocation from main.dart
-
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
@@ -34,16 +17,8 @@ class Category extends StatelessWidget {
     @required this.iconLocation,
   }) : super(key: key);
 
-  /// Builds a custom widget that shows [Category] information.
-  ///
-  /// This information includes the icon, name, and color for the [Category].
   @override
-  // This `context` parameter describes the location of this widget in the
-  // widget tree. It can be used for obtaining Theme data from the nearest
-  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
-  // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
-    // DONE: Build the custom widget here, referring to the Specs.
     return Material(
       color: Colors.transparent,
       child: Container(
