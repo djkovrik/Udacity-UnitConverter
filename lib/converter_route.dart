131e110b2e2862b46a9c6a5e7e3a908b3d0220fa
DJ_Kovrik <djkovrik@gmail.com>
2019-01-15 20:41:14 +0300

    Task 05: Stateful widgets

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -1,8 +1,12 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:udacity_unit_converter/unit.dart';
 
-class ConverterRoute extends StatelessWidget {
+class ConverterRoute extends StatefulWidget {
   final String name;
   final Color color;
   final List<Unit> units;
@@ -14,11 +18,16 @@ class ConverterRoute extends StatelessWidget {
   });
 
   @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
-        color: color,
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
